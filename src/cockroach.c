4bdab66cb19a97179e34133c065fd696967fcab1
Jose E. Marchesi <jemarch@gnu.org>
2011-10-31 14:40:12 +0100

    manual: initial stub.

--- src/cockroach.c
+++ src/cockroach.c
@@ -1,3 +1,28 @@
+/* -*- mode: C -*-
+ *
+ *       File:         cockroach.c
+ *       Date:         Mon Oct 31 14:27:16 CET 2011
+ *
+ *       GNU cockroach - Main source file.
+ *
+ */
+
+/* Copyright (C) 2011 Giuseppe Scrivano, Jose E. Marchesi */
+
+/* This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 int
 main (int argc, char *const *argv)
 {
