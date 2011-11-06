410ddb595af4cef7c1e6e34008708f2ce09b89cd
Jose E. Marchesi <jemarch@gnu.org>
2011-11-06 12:30:02 +0100

    Legal and minor cosmetic changes.

--- src/cockroach.c
+++ src/cockroach.c
@@ -1,27 +1,23 @@
-/* -*- mode: C -*-
- *
- *       File:         cockroach.c
- *       Date:         Mon Oct 31 14:27:16 CET 2011
- *
- *       GNU cockroach - Main source file.
- *
- */
+/*
+   cockroach utility.
 
-/* Copyright (C) 2011 Giuseppe Scrivano, Jose E. Marchesi */
+   Copyright (C) 2011 Jose E. Marchesi <jemarch@gnu.org>
+   Copyright (C) 2011 Giuseppe Scrivano <gscrivano@gnu.org>
 
-/* This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+   cockroach is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   GNU Wget is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with the program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "cockroach.h"
 #include <stdlib.h>
 #include <stdio.h>
